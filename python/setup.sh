7d227210fa13fc2e69333ad09ea1e2b3536b5ebf
Arya Kumar <2019akumar2@tjhsst.edu>
2018-08-24 06:55:18 +0300

    cleaned up python setup for nice comments and requests library

--- python/setup.sh
+++ python/setup.sh
@@ -1,22 +1,31 @@
-#!/bin/bash
+#!/usr/local/bin/zsh
+#
+# python
+#
+# Installs and configures python2 and python3 separately
+
+source ~/.zshrc
 
 # Upgrade the python installations
-/usr/local/bin/python2 -m pip install --quiet --upgrade pip setuptools wheel
-/usr/local/bin/python3 -m pip install --quiet --upgrade pip setuptools wheel
+green 'Updating python installations'
+python2 -m pip install --quiet --upgrade pip setuptools wheel
+python3 -m pip install --quiet --upgrade pip setuptools wheel
 
 # Remove the extra python2 installation
-rm -f /usr/local/bin/python
-rm -f /usr/local/bin/pip
+green 'Removing extra python install'
+rm -f /usr/local/bin/{python,pip}
 
 # Disallow a generic python command - force python2 or python3
-echo "#\!/bin/bash" >> /usr/local/bin/python
+green 'Disallowing generic python command'
+echo "#\!/bin/bash" > /usr/local/bin/python
 echo "echo Please specify python2 or python3" >> /usr/local/bin/python
 chmod +x /usr/local/bin/python
 
-echo "#\!/bin/bash" >> /usr/local/bin/pip
+echo "#\!/bin/bash" > /usr/local/bin/pip
 echo "echo Please specify pip2 or pip3" >> /usr/local/bin/pip
 chmod +x /usr/local/bin/pip
 
 # Install standard and common libraries
-/usr/local/bin/python2 -m pip install --quiet --user numpy scipy matplotlib requests
-/usr/local/bin/python2 -m pip install --quiet --user numpy scipy matplotlib requests
+green 'Installing standard libraries.'
+python2 -m pip install --quiet --user numpy scipy matplotlib requests
+python2 -m pip install --quiet --user numpy scipy matplotlib requests
