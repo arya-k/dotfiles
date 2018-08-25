4985f881f683262e3d75cd04d7d6bce0209cd546
Arya Kumar <2019akumar2@tjhsst.edu>
2018-08-25 17:14:25 -0400

    created install script

---
+++ bin/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# install.sh
+#
+# Installs everything, step by step.
+
+# First, instal cli tools
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+   xcode-select --install
+fi
+
+# Then, install homebrew stuff first:
+source ~/.dotfiles/brew/setup.sh
+
+# Next, install zsh
+source ~/.dotfiles/zsh/setup.sh
+
+# Next, switch to zsh:
+/usr/local/bin/zsh <<EOF
+
+# Install python:
+source ~/.dotfiles/python/setup.sh
+
+# Install hyperkey:
+source ~/.dotfiles/hyperkey/setup.sh
+
+# Install git:
+source ~/.dotfiles/git/setup.sh
+
+# Install app configs:
+source ~/.dotfiles/appconfigs/setup.sh
+
+# Install Desktop Wallpaper + Terminal Theme:
+source ~/.dotfiles/icons/setup.sh
+
+# Install macos config
+source ~/.dotfiles/macos/setup.sh
