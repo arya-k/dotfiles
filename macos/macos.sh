74085b92250c62ac9ba7bb61808090a4fbe101a3
Arya Kumar <thearyaskumar@icloud.com>
2019-06-30 18:32:28 -0400

    updated settings

--- macos/macos.sh
+++ macos/macos.sh
@@ -129,7 +129,7 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
