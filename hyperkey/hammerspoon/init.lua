e03acf62c18ff5a808c4aaab0e5c205fa87c177e
Arya Kumar <thearyaskumar@icloud.com>
2019-09-10 11:19:32 -0400

    updated settings

--- hyperkey/hammerspoon/init.lua
+++ hyperkey/hammerspoon/init.lua
@@ -29,38 +29,7 @@ end
 
 hs.hotkey.bind(hyper, "M", function()
     hs.osascript.applescript([[
-        # start by opening Safari:
-        tell application "Safari" to launch
-
-        # try to open the tab if it exists
-        tell application "Safari"
-            repeat with win in every window
-                repeat with t in every tab of win
-                    if "https://www.messenger.com" is in (URL of t as string) then
-                        set current tab of win to t
-                        set index of win to 1
-                        return
-                    end if
-                end repeat
-            end repeat
-        end tell
-
-        # try to open in an empty tab if it exists:
-        tell application "Safari"
-            repeat with win in every window
-                repeat with t in every tab of win
-                    if (URL of t as string) is equal to "favorites://" then
-                        set URL of t to "https://www.messenger.com/"
-                        set current tab of win to t
-                        set index of win to 1
-                        return
-                    end if
-                end repeat
-            end repeat
-        end tell
-
-        # otherwise, open the webpage
-        do shell script "open https://www.messenger.com"
+        on open_website()        	# start by opening Safari:        	tell application "Safari" to launch        	        	# try to open the tab if it exists        	tell application "Safari"        		repeat with win in every window        			repeat with t in every tab of win        				if "https://www.messenger.com" is in (URL of t as string) then        					set current tab of win to t        					set index of win to 1        					return        				end if        			end repeat        		end repeat        	end tell        	        	# try to open in an empty tab if it exists:        	tell application "Safari"        		repeat with win in every window        			repeat with t in every tab of win        				if (URL of t as string) is equal to "favorites://" then        					set URL of t to "https://www.messenger.com/"        					set current tab of win to t        					set index of win to 1        					return        				end if        			end repeat        		end repeat        	end tell        	        	# otherwise, open the webpage        	do shell script "open https://www.messenger.com"        end open_website                on wait_for_load()        	tell front document of application "Safari"        		activate        		repeat until length of (source as text) is not 0        			delay 0.5        		end repeat        	end tell        end wait_for_load                on auto_login()        	tell application "Safari"        		set at_login to (do JavaScript "document.getElementById('loginform') !== null;" in document 1)        	end tell        	        	if at_login then        		delay 0.2        		tell application "System Events" to tell application process "Safari"        			set app_position to position of window 1        			set app_size to size of window 1        			        			set x_top to item 1 of app_position        			set y_top to item 2 of app_position        			set x_range to item 1 of app_size        			set y_range to item 2 of app_size        			        			# enter the window        			click at {x_top + (x_range / 5), y_top + (y_range / 5)}        			        			# tab to the password prompt        			repeat        				tell application "Safari"        					if (do JavaScript "document.activeElement.type == 'password';" in document 1) then        						exit repeat        					end if        				end tell        				keystroke tab        				delay 0.2        			end repeat        			        			key code 125        			delay 0.1        			keystroke return        		end tell        	end if        end auto_login                # main logic:        on run        	open_website()        	wait_for_load()        	auto_login()        end run
     ]])
 end)
 
